0fb41d30d98b11fd0a3a0884544fb698d13a2b53
sariforest <iread1q84@gmail.com>
2022-01-02 17:47:33 +0900

    Fix misc.

--- resolvers.ts
+++ resolvers.ts
@@ -6,6 +6,6 @@ import {
 export const resolvers = {
     Query: {
         approveExportCart: (_, { id, exportApproved }) => approveExportCart(id, exportApproved),
-        hello: (_, hello: string) => test(),
+        hello: () => test(),
     },
 };

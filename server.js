ed3ad051383df75b97d6cc5bd51381657c772d0d
sariforest <iread1q84@gmail.com>
2022-01-02 09:45:27 +0900

    Try to use resolvers separately

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 import { GraphQLServer } from "graphql-yoga";
+import { resolvers } from "./resolvers";
 
 const typeDefs = "./schema.graphql";
-
-const resolvers = {
-    Query: {
-        hello: (_, hello) => "Hello World!",
-    },
-};
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => console.log("Running on localhost:4000"));
